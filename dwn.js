c3fe5886e2b82e0f0b765befb9f2aab1ef08c114
TheGummySoldier <marco.zheng.ca@gmail.com>
2023-04-06 19:04:58 +1200

    Cleaning up

--- dwn.js
+++ dwn.js
@@ -64,66 +64,10 @@ async function frontEnd(){
   {
     download(utils.cdnHosts[`${server}`], `movie2manifest_${server}`, `${server}`);
   }
-}
-//frontEnd();
+};
+frontEnd();
 process.on('uncaughtException', (err)=> console.log(err));
-download1(utils.cdnHosts["en"], "movie2manifest_en", "en");
 
 
 
-// something fun
 
-const Axios = require('axios')  
-const ProgressBar = require('progress')
-
-async function download1(cdnHost, filepath, server) {  
-  //url
-  const fileStream = fs.createReadStream(filepath);
-
-  const rl = readline.createInterface({
-    input: fileStream,
-    crlfDelay: Infinity
-  });
-
-  for await (const line of rl) {
-    let lineData = utils.parseManifestLine(line);
-    let hash = lineData.hash;
-    let url = "https://" + cdnHost + '/dl/pool/Movie/' + hash.substr(0, 2) + '/' + hash;
-    const dirPath = path.join(__dirname, server)
-    if (!fs.existsSync(dirPath)){
-      fs.mkdirSync(dirPath);
-  };
-  console.log('Connecting …')
-  const { data, headers } = await Axios({
-    url,
-    method: 'GET',
-    responseType: 'stream'
-  })
-  const totalLength = headers['content-length']
-
-  console.log('Starting download')
-  const progressBar = new ProgressBar(`-> downloading ${lineData.file} [:bar] :percent :etas \n`, {
-      width: 40,
-      complete: '|',
-      incomplete: ' ',
-      renderThrottle: 1,
-      total: parseInt(totalLength)
-    })
-
-    var outputPath = `${dirPath}/${lineData.file}`;
-    const writeStream = fs.createWriteStream(outputPath);
-
-    
-
-    
-  data.on('data', (chunk) => {
-    progressBar.tick(chunk.length)
-   console.log(`% of file ${lineData.file} completed`,(progressBar.curr/totalLength)*100)
-  })
-  data.pipe(writeStream);
-  writeStream.on("finish", () => {
-      writeStream.close();
-      console.log(`${lineData.file} downloaded!`);
-    })
-}
-}   
